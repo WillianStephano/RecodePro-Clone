d0157ef1be91df00d84101040c86c58c6e541fa6
Willian Stephano <will.stephano@gmail.com>
2022-03-21 17:44:25 -0300

    Criação de interatividade em menu principal

---
+++ Js/main.js
@@ -0,0 +1,33 @@
+window.onscroll = function() {menuFixo()};
+
+var barraMenu = document.querySelector('.cabecalho_principal')
+var sticky = barraMenu.offsetTop;//pego a distancia do elemento ate o topo da pagina
+var logo = document.querySelector('.img_logo_cabecalho')
+var txtMenu = barraMenu.querySelectorAll('.link_menu_lista')
+
+console.log(txtMenu);
+
+function menuFixo() {
+   if (window.pageYOffset >=sticky) {
+      barraMenu.classList.add("sticky")
+      logo.src ='Conteudo/recode_pro-NEG-logo.svg'
+      txtMenu[0].style.color = 'white'
+      txtMenu[1].style.color = 'white'
+      txtMenu[2].style.color = 'white'
+      txtMenu[3].style.color = 'white'
+      /* Devido a dificuldade fiz esta horrorossa gambiarra que logo sera corrigda */
+      
+   } else {
+      barraMenu.classList.remove("sticky");
+      logo.setAttribute ('src','Conteudo/recode_pro-logo-1.svg')
+      txtMenu[0].style.color = 'black'
+      txtMenu[1].style.color = 'black'
+      txtMenu[2].style.color = 'black'
+      txtMenu[3].style.color = 'black'
+      /* Devido a dificuldade fiz esta horrorossa gambiarra que logo sera corrigda */
+
+   }
+   
+}
+
+
